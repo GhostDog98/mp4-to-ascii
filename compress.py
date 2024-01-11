9fae999252aff38fa15b608d193db4c0b28ec229
GhostDog98 <46546505+GhostDog98@users.noreply.github.com>
2024-01-11 14:01:01 +1100

    reduced function calls to speed up by ~25%

--- compress.py
+++ compress.py
@@ -18,11 +18,6 @@ STORE_ON_DISK = False
 
 global video_fps      
 
-def get_video_fps(video_path):
-    capture = cv2.VideoCapture(video_path)
-    fps = capture.get(cv2.CAP_PROP_FPS)
-    capture.release()
-    return fps
 
 def extract_transform_generate(video_path, start_frame, end_frame, shared_list, frame_size):
      # Initialize shared list with empty strings for frames in the specified range
@@ -38,7 +33,7 @@ def extract_transform_generate(video_path, start_frame, end_frame, shared_list, 
         
         try:
             image = Image.fromarray(image_frame)
-            ascii_characters = pixels_to_ascii(greyscale(resize_image(image)))  # get ascii characters
+            ascii_characters = pixels_to_ascii(image)
             pixel_count = len(ascii_characters)
             ascii_image = "\n".join(
                 [ascii_characters[index:(index + frame_size)] for index in range(0, pixel_count, frame_size)])
@@ -57,57 +52,25 @@ def extract_transform_generate(video_path, start_frame, end_frame, shared_list, 
     capture.release()
 
 
-# Resize image
-def resize_image(image_frame):
+
+
+
+# Convert stuff
+# For some reason, merging these calls into one cuts down on ~25% of the runtime
+def pixels_to_ascii(image_frame):
+    # Convert to grayscale
+    image_frame = image_frame.convert("L")
+    
+    # Resize
     width, height = image_frame.size
     aspect_ratio = height / float(width * 2) 
     new_height = int(aspect_ratio * frame_size)
-    resized_image = image_frame.resize((frame_size, new_height))
-
-    return resized_image
-
-
-# Greyscale
-def greyscale(image_frame):
-    return image_frame.convert("L")
-
-
-# Convert pixels to ascii
-def pixels_to_ascii(image_frame):
+    image_frame = image_frame.resize((frame_size, new_height))
+    
+    # Convert to ascii
     pixels = image_frame.getdata()
     characters = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
     return characters
-
-
-# Open image => Resize => Greyscale => Convert to ASCII => Store in memory => Store in text file
-def ascii_generator(image_path, start_frame, number_of_frames):
-    ascii_images = []
-
-    # Calculate the delay for each frame based on the desired output FPS
-    frame_delay = 1 / video_fps
-
-    for current_frame in range(start_frame, number_of_frames + 1):
-        path_to_image = f"{image_path}/BadApple_{current_frame}.jpg"
-        image = Image.open(path_to_image)
-        
-        ascii_characters = pixels_to_ascii(greyscale(resize_image(image)))
-        
-        pixel_count = len(ascii_characters)
-        ascii_image = "\n".join(
-            [ascii_characters[index:(index + frame_size)] for index in range(0, pixel_count, frame_size)]
-        )
-        ascii_images.append(ascii_image)
-
-    if STORE_ON_DISK:
-        for idx, ascii_img in enumerate(ascii_images, start=start_frame):
-            file_name = f"TextFiles/bad_apple{idx}.txt"
-            try:
-                with open(file_name, "w", encoding='UTF-8') as f:
-                    f.write(ascii_img)
-            except FileNotFoundError:
-                continue
-            time.sleep(frame_delay)  # Introduce delay for desired output FPS
-
 
 
 def preflight_operations(path):
@@ -190,13 +153,21 @@ def rle_decode(data):
 
 MODE = 0
 def main():
+    """
     print("What's your video url (youtube)?")
     a = input("URL [e.g. https://www.youtube.com/watch?v=FtutLA63Cp8]: ")
     print("Run the command { yt-dlp -o file_to_encode.mp4 -f \"[height <=? 480]\" " + a + " }")
     input("Press enter once done")
+    """
     start_time = time.time()
     user_input = "file_to_encode.mp4"  
-    video_fps = get_video_fps(user_input)
+    
+    
+    # Get the fps
+    capture = cv2.VideoCapture(user_input)
+    video_fps = capture.get(cv2.CAP_PROP_FPS)
+    capture.release()
+    
     print("Encoding...")
     cap = cv2.VideoCapture(user_input)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -207,13 +178,14 @@ def main():
         elapsed = end_time - start_time
         print(f"{elapsed} seconds taken for generation, {(elapsed / total_frames)*1000}ms per frame")
         print("Converting to string object")
+        """
         with open("data.txt", 'w') as f:
             f.write('\n'.join(ASCII_LIST))
         print("Compressing...")
         compressed = compress(5, '\n'.join(ASCII_LIST))
         with open("compressed_data.zstd", "wb") as f:
             f.write(compressed)
-    
+        """
 
 
 def compress(algo: int, indata: str):
